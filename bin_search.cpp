dd630a3cba3cb6ba239a0f746712c8496e2927cf
ForSev4705 <90548679+Haianh4705@users.noreply.github.com>
2024-12-20 19:07:21 +0700

    Update bin_search.cpp

--- bin_search.cpp
+++ bin_search.cpp
@@ -11,13 +11,29 @@ bool cmp() {
 int bin_search(int l, int r) {
     while (l < r) {
         int m = l + (r-l)/2;
-        if (cmp(m)) {
+        // truyen m vao cmp
+        if (cmp()) {
             r = m;
         } else {
             l = m + 1;
         }
     }
     return l; // tra ve r (gia tri cuoi cung + 1) neu bai toan vo nghiem
+}
+
+// doi voi so thuc
+double bin_search(double l, double r) {
+    double e = 1e-8; // co the dung for 0 -> 100 neu de bai khong cho thoi gian chat
+    while (abs(l-r) >= e) {
+        double m = l + (r-l)/2;
+        // truyen m vao cmp
+        if (cmp()) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    return l + (r-l)/2;
 }
 
 int main() {
